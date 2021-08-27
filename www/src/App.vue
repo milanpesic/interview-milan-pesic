ab40ccaf65df6dd98cb7556281fd5b36b30e4ae1
milanpesic <milan.pesic.posao@gmail.com>
2021-08-27 04:31:50 +0200

    initial commit

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div>
     <to-do-list></to-do-list>
-    
   </div>
 </template>
 
 <script>
 
-/*import HelloWorld from './components/HelloWorld.vue'*/
 import ToDoList from './components/ToDoList.vue'
 
 export default {
   name: 'App',
   components: {
-   /* HelloWorld,*/
-    ToDoList,
+    ToDoList
   }
 }
 </script>
